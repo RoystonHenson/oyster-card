2fd6d670233d044a73cef4d008f749d91b77e81b
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-08-31 15:52:27 +0100

    Add starting balance to OysterCard

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,3 +1,7 @@
 class OysterCard
+  attr_accessor :balance
 
+  def initialize
+    @balance = 5.00
+  end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,8 +1,13 @@
 require 'coveralls'
 Coveralls.wear!
-
 require 'oyster_card'
 
 describe OysterCard do
+  let(:oyster_card) { OysterCard.new }
 
+  describe '#initialize' do
+    it 'has a starting balance of 5' do
+    expect(oyster_card.balance).to eq(5)
+    end
+  end
 end
