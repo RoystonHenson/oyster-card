15e704b00ca908c6c169c09b213bcfcedd45c330
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-03-28 23:22:51 +0000

    Add feature: Oystercard has a max balance that cannot be exceeded

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,12 +3,19 @@ class
     attr_reader :balance
 
   OPENING_BALANCE = 0
+  MAX_BALANCE = 90
 
   def initialize
     @balance = OPENING_BALANCE
   end
 
   def top_up(amount)
-    @balance += 1
+    verify_top_up_amount(amount)
+  end
+
+  private
+
+  def verify_top_up_amount(amount)
+    @balance + amount <= MAX_BALANCE ? @balance += amount : raise("This transaction would exceed the card limit of #{MAX_BALANCE}. Please top up a smaller amount.")
   end
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -10,9 +10,22 @@ describe OysterCard do
   end
 
   describe '#top_up' do
-    it 'adds money to the balance' do
-      oyster_card.top_up(1)
-      expect(oyster_card.balance).to eq(1)
+    context 'when card balance remains below balance limit' do
+      it 'adds money to the balance' do
+        oyster_card.top_up(1)
+        expect(oyster_card.balance).to eq(1)
+      end
+    end
+
+    context 'when top up will exceed balance limit' do
+      it 'will throw an error' do
+        expect { oyster_card.top_up(91) }.to raise_error(
+          RuntimeError, "This transaction would exceed the card limit of #{OysterCard::MAX_BALANCE}. Please top up a smaller amount.")
+      end
+
+      it 'will not add money to the current balance' do
+        expect { oyster_card.top_up(91) rescue nil }.not_to change { oyster_card.balance }
+      end
     end
   end
 end
