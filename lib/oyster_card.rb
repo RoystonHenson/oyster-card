e7bf0b3f27c90e88fd0c3af003eb6800aba57010
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-03-29 15:58:32 +0000

    Add feature: card can touch in, card can touch out, user can check if card is touched in or out

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,12 +1,13 @@
 class
   OysterCard
-    attr_reader :balance
+    attr_reader :balance, :in_journey
 
   OPENING_BALANCE = 0
   MAX_BALANCE = 90
 
   def initialize
     @balance = OPENING_BALANCE
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -15,6 +16,18 @@ class
 
   def deduct(fare)
    reduce_balance(fare)
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey == true
   end
 
   private
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -7,6 +7,10 @@ describe OysterCard do
     it 'has an opening balance of 0' do
       expect(oyster_card.balance).to eq(0)
     end
+
+    it 'is not touched in' do
+      expect(oyster_card.in_journey).to eq(false)
+    end
   end
 
   describe '#top_up' do
@@ -33,6 +37,38 @@ describe OysterCard do
     it 'subtracts a fare from the card\'s balance' do
       fare = 1
       expect { oyster_card.deduct(fare) }.to change { oyster_card.balance }.by(-fare)
+    end
+  end
+
+  describe '#touch_in' do
+    it 'touches a card in' do
+      oyster_card.touch_in
+      expect(oyster_card.in_journey).to eq (true)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'touches a card out' do
+      oyster_card.touch_in
+      oyster_card.touch_out
+      expect(oyster_card.in_journey).to eq(false)
+    end 
+  end
+
+  describe '#in_journey?' do
+    context 'when in journey' do  
+      it 'returns true' do
+        oyster_card.touch_in
+        expect(oyster_card).to be_in_journey
+      end
+    end
+
+    context 'when not in journey' do
+      it 'returns false' do
+        oyster_card.touch_in
+        oyster_card.touch_out
+        expect(oyster_card).not_to be_in_journey
+      end
     end
   end
 end
