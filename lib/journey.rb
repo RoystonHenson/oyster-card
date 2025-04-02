cfa9ad3eceaf649f97b931b1b8dd6bcbbb5c603e
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-04-02 16:46:36 +0100

    Add class-level history tracking for Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,13 @@
 class Journey
+  @@history = []
+
   attr_reader :current_journey
 
   def initialize(station)
     @current_journey = {entry_station: station}
+  end
+
+  def self.history
+    @@history
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,6 +4,12 @@ describe Journey do
   let(:station) { double('entry station')}
   let(:journey) { Journey.new(station) }
 
+  describe '.history' do
+    it 'is initialised as empty' do
+      expect(journey.class.history).to eq([])
+    end
+  end
+
   describe '#initialize' do
     it 'sets entry station on instantiation' do
       expect(journey.current_journey[:entry_station]).to eq(station)
