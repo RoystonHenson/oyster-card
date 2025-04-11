fa638b8a1314f8c90dda8cab8056d475d7378b78
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-04-11 14:58:16 +0100

    refactor(JourneyLog): move Journey#start to JourneyLog

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,15 +1,61 @@
 require_relative 'journey'
+require_relative 'station'
 
 class JourneyLog
-  attr_reader :journey_class
+  attr_reader :journey_class, :current_journey, :recent_journeys, :history
 
   def initialize(journey_class)
     @journey_class = journey_class
+    @current_journey = {entry_station: nil, exit_station: nil}
+    @recent_journeys = []
+    @history = []
   end
 
   def start(station)
     @journey_class.new(station)
+    raise('You have already touched in at this station!') if @current_journey[:entry_station] == station
+    if entry_station_unset?
+      start_journey(station)
+    else
+      start_penalty_journey(station)
+    end
+    #@current_journey[:entry_station] = station
+  end
+
+  def finish(station)
+
+  end
+
+  private
+
+  def entry_station_unset?
+    @current_journey[:entry_station].nil?
+  end
+
+  def start_journey(station)
+    reset_recent_journeys
+    set_entry_station(station)
+  end
+
+  def reset_recent_journeys
+    @recent_journeys = []
+  end
+
+  def set_entry_station(station)
+    @current_journey[:entry_station] = station
+  end
+
+  def start_penalty_journey(station)
+    apply_penalty_fare
+    save_journey
+    set_entry_station(station)
+  end
+
+  def apply_penalty_fare
+    warn "You failed to complete your last journey correctly. You will be charged £#{Journey::PENALTY_FARE} for this journey."
+  end
+
+  def save_journey
+    @recent_journeys << @current_journey.clone
   end
 end
-
-# starting a journey, ending a journey, returning a list of journeys
